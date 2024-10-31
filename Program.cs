b8e1c33e7caa3842cfdc0c2b52793e213189f465
Mathias <mathiasakesson@hotmail.com>
2024-10-31 15:50:23 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TestDevOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
